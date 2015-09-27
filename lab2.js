4c3f817d16cd0bddf82eb5d1b32d556c2e6221ab
Chaosman004 <mr.robert.page@gmail.com>
2015-09-26 23:35:08 -0400

    Third Commit

--- lab2.js
+++ lab2.js
@@ -1,49 +1,48 @@
-// node js Calculator
+// Simple Node.js Calculator, Robert Page 200281838
 var http = require('http');
 
 // require node's url module to parse the url's querystring
 var url = require('url');
 
 // create and load http server with node
-http.createServer(function(req, res) {
+http.createServer(function (req, res) {
     
     // get the querystring
     var qs = url.parse(req.url, true).query;
-       
-    // log to the console
-    console.log('working');
     
     // set the method and parse the x,y values as a float value
     var method = qs.method,
         x = parseFloat(qs.x),
         y = parseFloat(qs.y);
     
-        //check to see if the method is correct
-        if (method === "add" || method === "subtract" ||
+    //check to see if the method is correct
+    if (method === "add" || method === "subtract" ||
                 method === "multiply" || method === "divide") {
-            //change size of text to <h1>
-            res.write('<h1>');
-            //check which calculation to do
-            if (method === "add") {
-                res.write(x + ' + ' + y + ' = ' + (x + y));
-            } else if (method === "subtract") {
-                res.write(x + " - " + y + " = " + (x - y));
-            } else if (method === "multiply") {
-                res.write(x + " * " + y + " = " + (x * y));
-            } else {
-                res.write(x + " / " + y + " = " + (x / y));
-            }
+        //change size of text to <h1>
+        res.write('<h1>');
+        //check which calculation to do
+        if (method === "add") {
+            res.write(x + ' + ' + y + ' = ' + (x + y));
+            
+        } else if (method === "subtract") {
+            res.write(x + " - " + y + " = " + (x - y));
+            
+        } else if (method === "multiply") {
+            res.write(x + " * " + y + " = " + (x * y));
+            
         } else {
-            //out put method error message
-            res.write('<h1>Error!</h1><h3><br />'+
-                      'Method must be one of these four words...<br />'+
-                      '-add<br />-subtract<br />-multiply<br />-divide</h3>');
+            res.write(x + " / " + y + " = " + (x / y));
+            
         }
-    
+    } else {
+        //output method error message
+        res.write('<h1>Error!</h1><h3><br />' +
+                  'Method must be one of these four words...<br />' +
+                  '-add<br />-subtract<br />-multiply<br />-divide</h3>');
+    }
     
     // end the response
     res.end();
 
 }).listen(3000);
-
-// site will load at http://localhost:3000/lab2.js?method=addx=5&y=10
+// site will load at http://localhost:3000/lab2.js?method=add&x=5&y=10
